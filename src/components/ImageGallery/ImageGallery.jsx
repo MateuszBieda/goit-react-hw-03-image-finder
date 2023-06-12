a97ebd74ce86512146e37ef76b59743f84817410
MateuszBieda <mateusbieda@gmail.com>
2023-06-12 23:00:01 +0200

    update components

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+
+
+export class ImageGallery extends Component {
+  render() {
+    return (
+        <ul class="gallery">
+        {/* <!-- Zbiór <li> z obrazami --> */}
+      </ul>
+    );
+  }
+}
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,25 @@
+import { Component } from 'react';
+import css from './Searchbar.module.css'
+
+
+export class Searchbar extends Component {
+  render() {
+    return (
+      <header className={css.Searchbar}>
+        <form class="form">
+          <button type="submit" class="button">
+            <span class="button-label">Search</span>
+          </button>
+
+          <input
+            class="input"
+            type="text"
+            autocomplete="off"
+            autofocus
+            placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    );
+  }
+}
