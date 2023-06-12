a435caeb86dc6a58cdbdae3c414989ae698fc326
MateuszBieda <mateusbieda@gmail.com>
2023-06-12 23:03:18 +0200

    add components and css file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
-export const App = () => {
+import { Component } from 'react';
+import { Searchbar } from './Searchbar/Searchbar.jsx';
+// import { Button } from './Button/Button.jsx';
+// import { ImageGallery } from './ImageGallery/ImageGallery.jsx';
+// import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem.jsx';
+// import { Loader } from './Loader/Loader.jsx';
+// import { Modal } from './Modal/Modal.jsx';
+
+
+export class App extends Component {
+
+  render(){
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Searchbar/>
+      {/* <ImageGallery/>
+      <ImageGalleryItem/>
+      <Modal/>
+      <Button/>
+      <Loader/> */}
+
     </div>
   );
 };
+}
